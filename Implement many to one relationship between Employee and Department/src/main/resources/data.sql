3a6baf78b545a8319193cddb4cf1a01f79f42fb8
PRIYADHARSHINI K <priyasuguna2006@gmail.com>
2026-07-07 21:18:17 +0530

    Delete Implement many to one relationship between Employee and Department directory

--- Implement many to one relationship between Employee and Department/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO department (id, name) VALUES (1, 'Engineering');
-
-INSERT INTO employee (id, name, salary, em_dp_id) VALUES (1, 'Alice', 75000, 1);
-INSERT INTO employee (id, name, salary, em_dp_id) VALUES (2, 'Bob', 68000, 1);
