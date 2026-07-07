b97fce5d4279144905c6dc8cbfbe88d846feb6bb
PRIYADHARSHINI K <priyasuguna2006@gmail.com>
2026-07-07 21:18:27 +0530

    Delete Implement many to many relationship between Employee and Skill directory

--- Implement many to many relationship between Employee and Skill/src/main/java/com/example/demo/repository/SkillRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Skill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SkillRepository extends JpaRepository<Skill, Long> {}
