784c22d37ef4bc92e10ef0e082150284499a41a8
PRIYADHARSHINI K <priyasuguna2006@gmail.com>
2026-07-16 11:56:27 +0530

    WEEK 4

---
+++ WEEK 4/Microservices with Spring Boot 3 and Spring Cloud/Creating Microservices for account and loan/loan/src/main/java/com/cognizant/controller/LoanController.java
@@ -0,0 +1,25 @@
+package com.cognizant.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+
+    @GetMapping("/{number}")
+    public Map<String, Object> getLoan(@PathVariable String number) {
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("number", number);
+        response.put("type", "car");
+        response.put("loan", 400000);
+        response.put("emi", 3258);
+        response.put("tenure", 18);
+        return response;
+    }
+}
